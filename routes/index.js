c5d7e2e0981c32a65ee109fe72dd1aee2118012d
Sridhar Kumar Paneerselvam <spaneerselvam@paypal.com>
2015-10-28 00:12:55 -0500

    Added MongoDB config

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/helloworld', function(req, res, next) {
+  res.render('helloworld', { title: 'Sridhar Kumar' });
+});
+
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+    res.render('userlist', {
+      "userlist" : docs
+    });
+  });
+});
+
 module.exports = router;
